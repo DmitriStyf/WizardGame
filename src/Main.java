426f96ee7418b8676fafa3066b8371306c14726a
Dmitri Styf <Dmitri.styf@gmail.com>
2019-01-16 18:43:58 +0200

    initial commit2 created

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //System.out.println("Dear user, please enter name and surname!");
+
+        Wizard wizard = new Wizard("Dmitri", "Styf");
+        System.out.println("Hello " + wizard.name + " " + wizard.surname);
     }
 }
+
---
+++ src/Wizard.java
@@ -0,0 +1,28 @@
+
+public class Wizard {
+    String name;
+    String surname;
+
+
+    public Wizard(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
